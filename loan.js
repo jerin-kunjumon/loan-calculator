6125dffa6de955be3b796b44ffdf837f4df32363
jerin-kunjumon <jkjerryomalloor@gmail.com>
2024-01-29 16:12:03 +0530

    months-bugfix

--- loan.js
+++ loan.js
@@ -10,12 +10,12 @@ function Calculate() {
  
     // Calculating interest per month
     const interest = (amount * (rate * 0.01)) / months;
-    const totalInterest = 12*interest
+    const totalInterest = months*interest
     // Calculating total payment
     const monthlyPayment = ((amount / months) + interest).toFixed(2);
-    const totaPayment = monthlyPayment*12;
+    const totalPayment = monthlyPayment*months;
     
     document.querySelector("#totalInterest").innerHTML = "Total Interest : (₹) " + totalInterest;
     document.querySelector("#monthlyPayment").innerHTML = "Monthly Payment : (₹) " + monthlyPayment;
-    document.querySelector("#totalPayment").innerHTML = "Total Payment : (₹) " + totaPayment;
+    document.querySelector("#totalPayment").innerHTML = "Total Payment : (₹) " + totalPayment;
 }
